0d01cbbbbe1dd22bdc652b1878e0f234efa37140
Nakaya <eniehack@outlook.jp>
2024-11-20 16:59:37 +0900

    feat: header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,18 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<header>
+	<div class="flex justify-between p-4">
+		<h1>
+			<a href="/" class="hover:underline">ATMマップ</a>
+		</h1>
+		<nav class="flex space-x-4">
+			<a href="/about" class="hover:underline">About</a>
+			<a href="/contribute" class="hover:underline">貢献する</a>
+			<a href="https://github.com/eniehack/atm-map" class="hover:underline">github</a>
+		</nav>
+	</div>
+</header>
+<main>
+	{@render children()}
+</main>
