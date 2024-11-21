a5ad61c4d67de467ca47ad1dd83610edf67def05
Nakaya <eniehack@outlook.jp>
2024-11-21 15:09:00 +0900

    feat: 自販機mapからそれぞれページ内容をコピペしてきた

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,19 @@
+<article>
+	<h1>about</h1>
+	<section>
+		<h1>地図、ATMデータの出典</h1>
+		<p>
+			地図データ、ATM地点データ共に<a href="https://openstreetmap.org">OpenStreetMap</a
+			>を利用しています。 OSMの提供するデータはODbLの下にライセンスされます。 詳細は<a
+				href="https://www.openstreetmap.org/copyright">こちら</a
+			>をご覧ください。
+		</p>
+	</section>
+	<section>
+		<h1>プライバシーポリシー</h1>
+		<p>
+			このサービスではあなたの位置情報を取り扱います。
+			しかし、位置情報はあなたのブラウザのみに共有され、その他第三者に共有されることはありません。
+		</p>
+	</section>
+</article>
