f39431e4c6c6fc635015d410439b78edda85087d
Nakaya <eniehack@outlook.jp>
2024-11-22 22:24:39 +0900

    format

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 	let { children } = $props();
 </script>
+
 <header>
 	<div class="flex justify-between p-4 dark:bg-slate-800">
 		<h1>
@@ -10,7 +11,9 @@
 		<nav class="flex space-x-4">
 			<a href="/about" class="hover:underline dark:text-neutral-200">About</a>
 			<a href="/contribute" class="hover:underline dark:text-neutral-200">貢献する</a>
-			<a href="https://github.com/eniehack/atm-map" class="hover:underline dark:text-neutral-200">github</a>
+			<a href="https://github.com/eniehack/atm-map" class="hover:underline dark:text-neutral-200"
+				>github</a
+			>
 		</nav>
 	</div>
 </header>
