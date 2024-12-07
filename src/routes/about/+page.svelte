6022090f82acdeb902a816e23c59e83f802f0162
Nakaya <eniehack@outlook.jp>
2024-12-07 15:30:58 +0900

    feat: /about /contribute の装飾をした

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,19 +5,19 @@
 </script>
 
 <article>
-	<h1>about</h1>
-	<section>
-		<h1>地図、ATMデータの出典</h1>
+	<h1 class="p-5 text-2xl font-bold">about</h1>
+	<section class="p-4">
+		<h1 class="text-xl font-semibold">地図、ATMデータの出典</h1>
 		<p>
-			地図データ、ATM地点データ共に<a href="https://openstreetmap.org">OpenStreetMap</a
+			地図データ、ATM地点データ共に<a class="text-sky-500 hover:underline" href="https://openstreetmap.org">OpenStreetMap</a
 			>を利用しています。 OSMの提供するデータはODbLの下にライセンスされます。 詳細は<a
-				href="https://www.openstreetmap.org/copyright">こちら</a
+				class="text-sky-500 hover:underline" href="https://www.openstreetmap.org/copyright">こちら</a
 			>をご覧ください。
 		</p>
 	</section>
-	<section>
-		<h1>プライバシーポリシー</h1>
-		<p>
+	<section class="p-4">
+		<h1 class="text-xl font-semibold">プライバシーポリシー</h1>
+		<p class="p-3">
 			このサービスではあなたの位置情報を取り扱います。
 			しかし、位置情報はあなたのブラウザのみに共有され、その他第三者に共有されることはありません。
 		</p>
--- src/routes/contribute/+page.svelte
+++ src/routes/contribute/+page.svelte
@@ -4,11 +4,12 @@
 </script>
 
 <article>
-	<h1>このサイトへ貢献する</h1>
-	<section>
-		<h2>登録されていないATMの報告</h2>
-		<p>
+	<h1 class="p-5 text-2xl font-bold">このサイトへ貢献する</h1>
+	<section class="p-4">
+		<h2 class="text-xl font-semibold">登録されていないATMの報告</h2>
+		<p class="p-3">
 			このサイトでは、自動販売機のデータ、地図データを<a href="https://openstreetmap.org/"
+				class="text-sky-500 hover:underline"
 				>OpenStreetMap</a
 			>から取得しています。 OpenStreetMapにユーザ登録をして、ATMのデータを追加することもできます。
 		</p>
@@ -16,20 +17,18 @@
             または、Twitterハッシュタグ <a href="https://twitter.com/hashtag/itf_vendingmachine_map">#itf_vendingmachinemap</a> 、<a href="https://twitter.com/hashtag/%E7%AD%91%E6%B3%A2%E5%A4%A7%E5%AD%A6%E8%87%AA%E8%B2%A9%E6%A9%9FMap">#筑波大学自販機Map</a> を付けて投稿してください。その際は、「7A棟1F内」、「1A棟の1H棟・文サ館方面階段近く」などおおまかな場所を含め投稿してください。
         </p>-->
 	</section>
-	<section>
-		<h2>開発に参加する</h2>
-		<p>
-			このサイトは<a href="https://github.com/eniehack/atm-map">GitHub</a
+	<section class="p-4">
+		<h2 class="text-xl font-semibold">開発に参加する</h2>
+		<p class="p-3">
+			このサイトは<a class="text-sky-500 hover:underline" href="https://github.com/eniehack/atm-map">GitHub</a
 			>にて、オープンソースソフトウェアとして公開されています。 Pull Request歓迎です。
 		</p>
 	</section>
-	<section>
-		<h2>バグ等の報告、機能の提案</h2>
-		<p>
-			<a href="https://github.com/eniehack/atm-map/issues">GitHubのissue</a
-			>から、または、Twitterハッシュタグ
-			<a href="https://twitter.com/hashtag/atm-map">#itf_vendingmachinemap</a>
-			を付けて投稿してください。
+	<section class="p-4">
+		<h2 class="text-xl font-semibold">バグ等の報告、機能の提案</h2>
+		<p class="p-3">
+			<a class="text-sky-500 hover:underline" href="https://github.com/eniehack/atm-map/issues">GitHubのissue</a
+			>に投稿してください。
 		</p>
 	</section>
 </article>
